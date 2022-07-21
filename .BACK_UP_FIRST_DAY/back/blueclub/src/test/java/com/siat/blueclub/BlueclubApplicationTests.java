99574e1dfe4ec7ffd749f4a13a13fb6e0659a622
Azabell1993 <moreirly@naver.com>
2022-07-21 11:21:50 +0900

    delete_gitingnore

---
+++ .BACK_UP_FIRST_DAY/back/blueclub/src/test/java/com/siat/blueclub/BlueclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siat.blueclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
