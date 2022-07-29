207d19c9c8fba8c371374f12931518f52d969905
ds3569 <76651279+ds3569@users.noreply.github.com>
2022-07-29 15:52:22 +0900

    Delete BackEnd/huisang/blueclub directory

--- BackEnd/huisang/blueclub/src/main/java/com/siat/blueclub/persistence/RequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.siat.blueclub.persistence;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.siat.blueclub.domain.Request;
-
-public interface RequestRepository extends CrudRepository<Request, Long>{
-
-}
