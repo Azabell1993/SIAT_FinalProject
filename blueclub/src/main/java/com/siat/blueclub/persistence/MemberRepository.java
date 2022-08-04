7d7f0410be4de80582d9b0f105b634ec200450fe
ds3569 <76651279+ds3569@users.noreply.github.com>
2022-08-04 20:57:15 +0900

    Delete blueclub directory

--- blueclub/src/main/java/com/siat/blueclub/persistence/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.siat.blueclub.persistence;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.siat.blueclub.domain.Member;
-
-public interface MemberRepository extends CrudRepository<Member, String> {
-	
-}
